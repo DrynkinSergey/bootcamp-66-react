5c624f1bf42ca39081af6f498ad3a7849f80429d
Sergey <sergsmile29@gmail.com>
2024-01-09 10:15:15 +0200

    uncontrolled form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import { TodoList } from './components/TodoList/TodoList'
 import { Employee } from './components/Employees/Employee'
 import { RegisterForm } from './components/RegisterForm/RegisterForm'
 import './index.css'
+import { RegisterFormUncontrolled } from './components/RegisterForm/RegisterFormUncontrolled'
 export const App = () => {
+	const register = data => {
+		console.log('Sending data...')
+		setTimeout(() => {
+			console.log(data)
+			console.log('User has been registered!!')
+		}, 2000)
+	}
 	return (
 		<>
-			<Employee />
-			<RegisterForm />
+			{/* <Employee /> */}
+			<RegisterFormUncontrolled register={register} />
 		</>
 	)
 }
