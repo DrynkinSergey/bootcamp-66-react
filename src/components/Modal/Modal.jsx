826b250d80386c7d36098042a7a497aa05969b8e
Sergey <sergsmile29@gmail.com>
2024-01-10 12:03:43 +0200

    interval + timeout and remove it with component will unmount

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,18 +1,77 @@
+import React from 'react'
 import { CloseButton, ModalContent, ModalWrapper } from './Modal.styled'
 
-const Modal = ({ children, close }) => {
-	return (
-		<ModalWrapper>
-			<ModalContent>
-				<>
-					<h1>Modal</h1>
-					<hr />
-				</>
-				<CloseButton onClick={close}>×</CloseButton>
-				{children}
-			</ModalContent>
-		</ModalWrapper>
-	)
+class Modal extends React.Component {
+	timeoutID = null
+	intervalID = null
+
+	componentDidMount() {
+		document.addEventListener('keydown', this.handleKeyDown)
+
+		this.timeoutID = setTimeout(() => {
+			console.log('Badabum!')
+		}, 3000)
+
+		this.intervalID = setInterval(() => {
+			console.log(new Date().toLocaleTimeString())
+		}, 1000)
+	}
+
+	componentWillUnmount() {
+		console.log('Modal was closed')
+		// Знімаємо натискання на клавіши
+		document.removeEventListener('keydown', this.handleKeyDown)
+
+		// Знімаємо інтервал та таймаут
+		clearTimeout(this.timeoutID)
+		clearInterval(this.intervalID)
+	}
+
+	handleClickOnBackdrop = e => {
+		// console.log('current target =>> ', e.currentTarget)
+		// console.log('target =>> ', e.target)
+		// this.props.close()
+		if (e.currentTarget === e.target) {
+			this.props.close()
+		}
+	}
+
+	handleKeyDown = e => {
+		console.log(e.key)
+		if (e.key === 'Escape') {
+			this.props.close()
+		}
+	}
+
+	render() {
+		const { close, children } = this.props
+		return (
+			<ModalWrapper onClick={this.handleClickOnBackdrop}>
+				<ModalContent>
+					<>
+						<h1>Modal</h1>
+						<hr />
+					</>
+					<CloseButton onClick={close}>×</CloseButton>
+					{children}
+				</ModalContent>
+			</ModalWrapper>
+		)
+	}
 }
+// const Modal = ({ children, close }) => {
+// return (
+// 	<ModalWrapper>
+// 		<ModalContent>
+// 			<>
+// 				<h1>Modal</h1>
+// 				<hr />
+// 			</>
+// 			<CloseButton onClick={close}>×</CloseButton>
+// 			{children}
+// 		</ModalContent>
+// 	</ModalWrapper>
+// )
+// }
 
 export default Modal
