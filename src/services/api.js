febff8b9d2ec203203d36bf87ec6597c0105d2ab
Sergey <sergsmile29@gmail.com>
2024-01-11 12:31:50 +0200

    added func searchByQuery

---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://dummyjson.com/'
+
+export const fetchPosts = async configParams => {
+	const { data } = await axios.get('posts', {
+		params: {
+			...configParams,
+		},
+	})
+	return data
+}
+
+export const fetchPostsByQuery = async configParams => {
+	const { data } = await axios.get('posts/search', {
+		params: {
+			...configParams,
+		},
+	})
+	return data
+}
