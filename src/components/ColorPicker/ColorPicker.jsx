3b0ec5f033deaa050dbff669af745c7b9182428b
Sergey <sergsmile29@gmail.com>
2024-01-15 10:58:29 +0200

    employee useState and colorPicker same this

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,47 +1,69 @@
-import React, { Component, PureComponent } from 'react'
+import React, { Component, PureComponent, useState } from 'react'
 import { StyledBackgroundTheme, StyledColorPalette, StyledColor, StyledColorsList } from './ColorPicker.styled'
 
-export class ColorPicker extends PureComponent {
-	// Якщо ми не передамо пропс кольорів - буде використано цей об'єкт
-	static defaultProps = {
-		colors: [{ id: 1, color: 'blue' }],
+export const ColorPicker = ({ colors }) => {
+	const [currentColor, setCurrentColor] = useState('white')
+	const handlePickColor = color => {
+		// this.setState({ currentColor: color })
+		setCurrentColor(color)
 	}
-
-	state = {
-		currentColor: 'white',
-	}
-
-	// shouldComponentUpdate(nextProps, nextState) {
-	// 	return nextState.currentColor !== this.state.currentColor
-	// }
-
-	componentDidUpdate(prevProps, prevState) {
-		console.log('Update: ', this.state.currentColor)
-	}
-
-	handlePickColor = color => {
-		this.setState({ currentColor: color })
-	}
-
-	render() {
-		const { colors } = this.props
-		const { currentColor } = this.state
-		return (
-			<StyledBackgroundTheme $bgColor={currentColor}>
-				<h1>Now we have color: {currentColor}</h1>
-				<StyledColorPalette>
-					<StyledColorsList>
-						{colors.map(item => (
-							<StyledColor onClick={() => this.handlePickColor(item.color)} key={item.id}>
-								{item.color}
-							</StyledColor>
-						))}
-					</StyledColorsList>
-				</StyledColorPalette>
-			</StyledBackgroundTheme>
-		)
-	}
+	return (
+		<StyledBackgroundTheme $bgColor={currentColor}>
+			<h1>Now we have color: {currentColor}</h1>
+			<StyledColorPalette>
+				<StyledColorsList>
+					{colors.map(item => (
+						<StyledColor onClick={() => handlePickColor(item.color)} key={item.id}>
+							{item.color}
+						</StyledColor>
+					))}
+				</StyledColorsList>
+			</StyledColorPalette>
+		</StyledBackgroundTheme>
+	)
 }
+
+// export class ColorPicker extends PureComponent {
+// 	// Якщо ми не передамо пропс кольорів - буде використано цей об'єкт
+// 	static defaultProps = {
+// 		colors: [{ id: 1, color: 'blue' }],
+// 	}
+
+// 	state = {
+// 		currentColor: 'white',
+// 	}
+
+// 	// shouldComponentUpdate(nextProps, nextState) {
+// 	// 	return nextState.currentColor !== this.state.currentColor
+// 	// }
+
+// 	componentDidUpdate(prevProps, prevState) {
+// 		console.log('Update: ', this.state.currentColor)
+// 	}
+
+// 	handlePickColor = color => {
+// 		this.setState({ currentColor: color })
+// 	}
+
+// 	render() {
+// 		const { colors } = this.props
+// 		const { currentColor } = this.state
+// 		return (
+// 			<StyledBackgroundTheme $bgColor={currentColor}>
+// 				<h1>Now we have color: {currentColor}</h1>
+// 				<StyledColorPalette>
+// 					<StyledColorsList>
+// 						{colors.map(item => (
+// 							<StyledColor onClick={() => this.handlePickColor(item.color)} key={item.id}>
+// 								{item.color}
+// 							</StyledColor>
+// 						))}
+// 					</StyledColorsList>
+// 				</StyledColorPalette>
+// 			</StyledBackgroundTheme>
+// 		)
+// 	}
+// }
 
 // export const ColorPicker = ({ colors = [] }) => {
 // return (
--- src/components/Employees/Employee.jsx
+++ src/components/Employees/Employee.jsx
@@ -1,105 +1,180 @@
 import { EmployeesFilter } from './EmployeesFilter'
 import userData from './../../assets/users.json'
-import React from 'react'
+import React, { useState } from 'react'
 import { getFilteredData } from '../../helpers/getFilteredData'
 import { EmployeeList } from './EmployeeList'
 import { toast } from 'react-toastify'
 import Modal from '../Modal/Modal'
 
-export class Employee extends React.Component {
-	state = {
-		users: userData,
-		filterStr: '',
-		isAvailable: false,
-		activeSkill: 'all',
-		isOpen: false,
-		modalContent: null,
+export const Employee = () => {
+	const [users, setUsers] = useState(userData)
+	const [filterStr, setFilterStr] = useState('')
+	const [isAvailable, setIsAvailable] = useState(false)
+	const [activeSkill, setActiveSkill] = useState('all')
+	const [isOpen, setIsOpen] = useState(false)
+	const [modalContent, setModalContent] = useState(null)
+
+	const handleOpenModal = () => {
+		// this.setState({ isOpen: true })
+		setIsOpen(true)
+	}
+	const handleCloseModal = () => {
+		// this.setState({ isOpen: false })
+		setIsOpen(false)
 	}
 
-	componentDidMount() {
-		const users = JSON.parse(localStorage.getItem('USERS_DATA'))
-		if (users?.length) {
-			this.setState({ users })
-		}
+	const handleSetModalContent = content => {
+		// this.setState({ isOpen: true, modalContent: content })
+		setIsOpen(true)
+		setModalContent(content)
 	}
 
-	componentDidUpdate(_, prevState) {
-		if (prevState.users.length !== this.state.users.length) {
-			toast.info('Users were changed')
-			localStorage.setItem('USERS_DATA', JSON.stringify(this.state.users))
-		}
+	const handleChangeActiveSkill = skill => {
+		// this.setState({ activeSkill: skill })
+		setActiveSkill(skill)
+	}
+	const handleClearFilter = () => {
+		// this.setState({ filterStr: '' })
+		setFilterStr('')
 	}
 
-	// Methods for modal
-	handleOpenModal = () => {
-		this.setState({ isOpen: true })
-	}
-	handleCloseModal = () => {
-		this.setState({ isOpen: false })
+	const handleChangeIsAvailable = () => {
+		// this.setState({ isAvailable: !this.state.isAvailable })
+		setIsAvailable(prev => !prev)
 	}
 
-	handleSetModalContent = content => {
-		this.setState({ isOpen: true, modalContent: content })
+	const handleChangeFilterStr = e => {
+		// this.setState({ filterStr: e.target.value })
+		setFilterStr(e.target.value)
 	}
 
-	handleChangeActiveSkill = skill => {
-		this.setState({ activeSkill: skill })
+	const handleDeleteUser = id => {
+		setUsers(prev => prev.filter(user => user.id !== id))
+		// this.setState(prevState => ({ users: prevState.users.filter(user => user.id !== id) }))
 	}
-	handleClearFilter = () => {
-		this.setState({ filterStr: '' })
-	}
+	const filteredUsers = getFilteredData(users, filterStr, isAvailable, activeSkill)
+	return (
+		<>
+			<EmployeesFilter
+				onChangeActiveSkill={handleChangeActiveSkill}
+				onChangeIsAvalaible={handleChangeIsAvailable}
+				onChangeFilter={handleChangeFilterStr}
+				onClearFilter={handleClearFilter}
+				filterStr={filterStr}
+				activeSkill={activeSkill}
+				isAvailable={isAvailable}
+			/>
+			<button onClick={handleOpenModal}>Open MODAL</button>
+			{isOpen && (
+				<Modal title={modalContent.name} close={handleCloseModal}>
+					<h1>{modalContent.bio}</h1>
+					<h2>{modalContent.isOpenToWork ? 'хоче працювати' : 'не хоче працювати'}</h2>
+				</Modal>
+			)}
 
-	handleChangeIsAvailable = () => {
-		this.setState({ isAvailable: !this.state.isAvailable })
-	}
-
-	handleChangeFilterStr = e => {
-		this.setState({ filterStr: e.target.value })
-	}
-
-	handleDeleteUser = id => {
-		this.setState(prevState => ({ users: prevState.users.filter(user => user.id !== id) }))
-	}
-
-	render() {
-		const { filterStr, isAvailable, activeSkill, users } = this.state
-		const filteredUsers = getFilteredData(users, filterStr, isAvailable, activeSkill)
-
-		return (
-			<>
-				<EmployeesFilter
-					onChangeActiveSkill={this.handleChangeActiveSkill}
-					onChangeIsAvalaible={this.handleChangeIsAvailable}
-					onChangeFilter={this.handleChangeFilterStr}
-					onClearFilter={this.handleClearFilter}
-					filterStr={filterStr}
-					activeSkill={activeSkill}
-					isAvailable={isAvailable}
-				/>
-				<button onClick={this.handleOpenModal}>Open MODAL</button>
-				{this.state.isOpen && (
-					<Modal title={this.state.modalContent.name} close={this.handleCloseModal}>
-						<h1>{this.state.modalContent.bio}</h1>
-						<h2>{this.state.modalContent.isOpenToWork ? 'хоче працювати' : 'не хоче працювати'}</h2>
-					</Modal>
-				)}
-				{/* {this.state.isOpen && (
-					<Modal close={this.handleCloseModal}>
-						<h1>Продам холодильник!</h1>
-						<img
-							src='https://content.rozetka.com.ua/goods/images/big/277445401.jpg'
-							alt='fridge'
-							width={400}
-							height={500}
-						/>
-					</Modal>
-				)} */}
-				<EmployeeList
-					users={filteredUsers}
-					onDeleteUser={this.handleDeleteUser}
-					openModalWithContent={this.handleSetModalContent}
-				/>
-			</>
-		)
-	}
+			<EmployeeList
+				users={filteredUsers}
+				onDeleteUser={handleDeleteUser}
+				openModalWithContent={handleSetModalContent}
+			/>
+		</>
+	)
 }
+
+// export class Employee extends React.Component {
+// 	state = {
+// 		users: userData,
+// 		filterStr: '',
+// 		isAvailable: false,
+// 		activeSkill: 'all',
+// 		isOpen: false,
+// 		modalContent: null,
+// 	}
+
+// 	componentDidMount() {
+// 		const users = JSON.parse(localStorage.getItem('USERS_DATA'))
+// 		if (users?.length) {
+// 			this.setState({ users })
+// 		}
+// 	}
+
+// 	componentDidUpdate(_, prevState) {
+// 		if (prevState.users.length !== this.state.users.length) {
+// 			toast.info('Users were changed')
+// 			localStorage.setItem('USERS_DATA', JSON.stringify(this.state.users))
+// 		}
+// 	}
+
+// 	// Methods for modal
+// 	handleOpenModal = () => {
+// 		this.setState({ isOpen: true })
+// 	}
+// 	handleCloseModal = () => {
+// 		this.setState({ isOpen: false })
+// 	}
+
+// 	handleSetModalContent = content => {
+// 		this.setState({ isOpen: true, modalContent: content })
+// 	}
+
+// 	handleChangeActiveSkill = skill => {
+// 		this.setState({ activeSkill: skill })
+// 	}
+// 	handleClearFilter = () => {
+// 		this.setState({ filterStr: '' })
+// 	}
+
+// 	handleChangeIsAvailable = () => {
+// 		this.setState({ isAvailable: !this.state.isAvailable })
+// 	}
+
+// 	handleChangeFilterStr = e => {
+// 		this.setState({ filterStr: e.target.value })
+// 	}
+
+// 	handleDeleteUser = id => {
+// 		this.setState(prevState => ({ users: prevState.users.filter(user => user.id !== id) }))
+// 	}
+
+// 	render() {
+// 		const { filterStr, isAvailable, activeSkill, users } = this.state
+// 		const filteredUsers = getFilteredData(users, filterStr, isAvailable, activeSkill)
+
+// 		return (
+// 			<>
+// 				<EmployeesFilter
+// 					onChangeActiveSkill={this.handleChangeActiveSkill}
+// 					onChangeIsAvalaible={this.handleChangeIsAvailable}
+// 					onChangeFilter={this.handleChangeFilterStr}
+// 					onClearFilter={this.handleClearFilter}
+// 					filterStr={filterStr}
+// 					activeSkill={activeSkill}
+// 					isAvailable={isAvailable}
+// 				/>
+// 				<button onClick={this.handleOpenModal}>Open MODAL</button>
+// 				{this.state.isOpen && (
+// 					<Modal title={this.state.modalContent.name} close={this.handleCloseModal}>
+// 						<h1>{this.state.modalContent.bio}</h1>
+// 						<h2>{this.state.modalContent.isOpenToWork ? 'хоче працювати' : 'не хоче працювати'}</h2>
+// 					</Modal>
+// 				)}
+// 				{/* {this.state.isOpen && (
+// 					<Modal close={this.handleCloseModal}>
+// 						<h1>Продам холодильник!</h1>
+// 						<img
+// 							src='https://content.rozetka.com.ua/goods/images/big/277445401.jpg'
+// 							alt='fridge'
+// 							width={400}
+// 							height={500}
+// 						/>
+// 					</Modal>
+// 				)} */}
+// 				<EmployeeList
+// 					users={filteredUsers}
+// 					onDeleteUser={this.handleDeleteUser}
+// 					openModalWithContent={this.handleSetModalContent}
+// 				/>
+// 			</>
+// 		)
+// 	}
+// }
