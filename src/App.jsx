edf0c2b17e2d26c2b4f9562651f6f5e3901f41d8
Sergey <sergsmile29@gmail.com>
2024-01-16 22:39:20 +0200

    create m5l1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import React, { createContext } from 'react'
-import { Employee } from './components/Employees/Employee'
-import { TodoList } from './components/TodoList/TodoList'
-import { Counter } from './components/Counter/Counter'
-import { ColorPicker } from './components/ColorPicker/ColorPicker'
-import colors from './assets/colors.json'
 import './index.css'
-import Posts from './components/Posts/Posts'
-import { Header } from './components/Header/Header'
 
 export const App = () => {
-	const message = 'hello'
-	return (
-		<>
-			<Header />
-			<Posts message={message} />
-			{/* <Counter /> */}
-		</>
-	)
+	return <></>
 }
