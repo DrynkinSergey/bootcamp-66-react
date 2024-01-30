291ab1864a35b0db2da0df7cf0dad2a370809e1d
Sergey <sergsmile29@gmail.com>
2024-01-30 13:06:28 +0200

    motion, active navlink tailwind

--- src/pages/Yup.jsx
+++ src/pages/Yup.jsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { schema } from '../schemas/registerSchema'
 import { Input, PasswordInput } from '../components/Forms/Input'
-
+import { motion } from 'framer-motion'
 export const Yup = () => {
 	const {
 		register,
@@ -20,15 +20,37 @@ export const Yup = () => {
 	return (
 		<div className='hero min-h-screen bg-base-200'>
 			<div className='hero-content flex-col lg:flex-row-reverse'>
-				<div className='text-center lg:text-left'>
+				<motion.div
+					animate={{
+						opacity: 1,
+						x: 0,
+						transition: {
+							duration: 0.4,
+							delay: 0.5,
+						},
+					}}
+					initial={{ opacity: 0, x: 300 }}
+					className='text-center lg:text-left'
+				>
 					<h1 className='text-5xl font-bold'>Yup</h1>
 					<p className='py-6'>
 						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ad laudantium ex. Nam molestiae et
 						velit assumenda, consectetur sint vel qui ullam ducimus dolor commodi cupiditate laboriosam voluptates
 						temporibus eius.
 					</p>
-				</div>
-				<div className='card shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
+				</motion.div>
+				<motion.div
+					initial={{ opacity: 0, scale: 0 }}
+					animate={{
+						opacity: 1,
+						scale: 1,
+						transition: {
+							duration: 0.4,
+							delay: 0.5,
+						},
+					}}
+					className='card shrink-0 w-full max-w-sm shadow-2xl bg-base-100'
+				>
 					<form onSubmit={handleSubmit(submit)} className='card-body relative'>
 						<Input register={register} name='name' placeholder='Enter your name' errors={errors} label='Name' />
 						<Input register={register} name='email' placeholder='Enter your email' errors={errors} label='Email' />
@@ -62,7 +84,7 @@ export const Yup = () => {
 							<button className='btn btn-primary'>Register</button>
 						</div>
 					</form>
-				</div>
+				</motion.div>
 			</div>
 		</div>
 	)
